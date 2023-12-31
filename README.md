b47c3824007fdba993fea493df79943383212663
deflax <daniel@deflax.net>
2023-12-31 02:30:04 +0000

    migrate from dev

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# television
-multi channel stream setup
+# Television
+
+Multi channel stream setup
+
+# stream-scheduler
+
+A Python REST API based on Flask for scheduling streaming channels
---
+++ gen-selfsigned.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p data/certificates
+cd data/certificates
+openssl genrsa -out default.key 2048
+openssl req -new -key default.key -out default.csr
+openssl x509 -req -days 3650 -in default.csr -signkey default.key -out default.crt
+cat default.key default.crt >> default.pem
+rm default.key
+rm default.csr
+rm default.crt
