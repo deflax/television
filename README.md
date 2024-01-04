141e31bc443b9947027f6bff9947f04ab942dca2
deflax <daniel@deflax.net>
2024-01-04 22:57:03 +0200

    set init script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Television
 
 Multi channel stream setup with Flask REST API for scheduling channels.
+
+1. run `./init.sh` to prepare data directories and generate self signed certs to bootstrap the load balancer
+2. `cp variables.env.dist variables.env` and set the required variables
+3. run `./renew-certificates.sh` periodically to generate/update the certificates
+4. run the docker-compose stack using `docker-compose up -d --build --remove-orphans`
+5. access the admin panel at `https://stream.example.com/ui`
---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "creating data dir structure"
+mkdir -v -p data/restreamer/config
+mkdir -v -p data/restreamer/data
+
+mkdir -v -p data/recorder/vod
+mkdir -v -p data/recorder/live
+
+mkdir -v -p data/archive
+
+mkdir -v -p data/certbot/etc
+mkdir -v -p data/certbot/var
+mkdir -v -p logs/certbot
+
+mkdir -v -p data/certificates
+
+echo "generating self signed certificates for haproxy bootstrap"
+cd data/certificates
+openssl genrsa -out default.key 2048
+openssl req -new -key default.key -out default.csr
+openssl x509 -req -days 3650 -in default.csr -signkey default.key -out default.crt
+cat default.key default.crt >> default.pem
+rm default.key
+rm default.csr
+rm default.crt
