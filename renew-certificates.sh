4ef765508d36acdfadef0c2fc087f6c05d9d9221
deflax <daniel@deflax.net>
2024-01-19 05:38:19 +0200

    remove xemle/home-gallery

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -8,7 +8,7 @@ CB=`docker ps | grep certbot | cut -d ' ' -f 1`
 #echo $EMAIL
 
 docker exec $CB certbot certonly --non-interactive --standalone --http-01-address 0.0.0.0 --email $EMAIL --agree-tos --keep --preferred-challenges http --cert-name stream.$BASE_URL \
-	-d tv.$BASE_URL -d stream.$BASE_URL -d vod.$BASE_URL
+	-d tv.$BASE_URL -d stream.$BASE_URL
 
 cat "./data/certbot/etc/live/stream.$BASE_URL/privkey.pem" "./data/certbot/etc/live/stream.$BASE_URL/fullchain.pem" > "./data/certificates/stream.$BASE_URL.pem"
 docker kill -s USR2 television_haproxy_1
