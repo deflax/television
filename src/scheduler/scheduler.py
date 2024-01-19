83ece2c59e11005f6b9edd6ee5707de0294ba67d
deflax <daniel@deflax.net>
2024-01-20 01:44:16 +0200

    enable sys.exit

--- src/scheduler/scheduler.py
+++ src/scheduler/scheduler.py
@@ -280,7 +280,7 @@ try:
 except Exception as err:
     logger_api.error('Client login error')
     logger_api.error(err)
-    #sys.exit(1)
+    sys.exit(1)
     
 # Schedule sync jobs
 scheduler.add_job(func=core_api_sync, trigger='interval', seconds=core_sync_period, id='core_api_sync')
