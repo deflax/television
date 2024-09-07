b861238170ec7f08b656b76f3e85a4bbb0d6a9b0
deflax <daniel@deflax.net>
2024-09-07 14:40:06 +0000

    rename the scheduler service to api

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -7,8 +7,8 @@ CB=`docker ps | grep certbot | cut -d ' ' -f 1`
 #echo $BASE_URL
 #echo $EMAIL
 
-docker exec $CB certbot certonly --non-interactive --standalone --http-01-address 0.0.0.0 --email $EMAIL --agree-tos --keep --preferred-challenges http --cert-name stream.$BASE_URL \
-	-d tv.$BASE_URL -d stream.$BASE_URL
+docker exec $CB certbot certonly --non-interactive --standalone --http-01-address 0.0.0.0 --email $EMAIL --agree-tos --keep --preferred-challenges http --cert-name $BASE_URL \
+	-d $BASE_URL -d api.$BASE_URL -d stream.$BASE_URL
 
-cat "./data/certbot/etc/live/stream.$BASE_URL/privkey.pem" "./data/certbot/etc/live/stream.$BASE_URL/fullchain.pem" > "./data/certificates/stream.$BASE_URL.pem"
+cat "./data/certbot/etc/live/$BASE_URL/privkey.pem" "./data/certbot/etc/live/$BASE_URL/fullchain.pem" > "./data/certificates/$BASE_URL.pem"
 docker kill -s USR2 television_haproxy_1
