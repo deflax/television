05f5f3b95c7327884bc0377ca9fdb3f1b8d8d766
deflax <daniel@deflax.net>
2025-01-28 18:13:50 +0000

    Replace certbot with acme.sh container

--- init.sh
+++ init.sh
@@ -2,11 +2,8 @@
 
 echo "creating data dir structure"
 
-# certbot
-mkdir -v -p data/certbot/etc
-mkdir -v -p data/certbot/var
-mkdir -v -p logs/certbot
-
+# acme.sh
+mkdir -v -p data/acme
 mkdir -v -p data/certificates
 
 # restreamer
@@ -24,13 +21,3 @@ touch logs/icecast/access.log
 touch logs/icecast/error.log
 chown 1000:1000 logs/icecast/access.log
 chown 1000:1000 logs/icecast/error.log
-
-echo "generating self signed certificates for haproxy bootstrap"
-cd data/certificates
-openssl genrsa -out default.key 2048
-openssl req -new -key default.key -out default.csr
-openssl x509 -req -days 3650 -in default.csr -signkey default.key -out default.crt
-cat default.key default.crt >> default.pem
-rm default.key
-rm default.csr
-rm default.crt
