392c23afe1d0b8b35c9bf14ddd9cb07caca52283
deflax <daniel@deflax.net>
2025-02-06 19:45:24 +0200

    fix a typo

--- src/api/scripts/entrypoint.sh
+++ src/api/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MODULE_NAME=${MODULE_NAME:-$flask_api}
+MODULE_NAME=${MODULE_NAME:-flask_api}
 VARIABLE_NAME=${VARIABLE_NAME:-create_app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
