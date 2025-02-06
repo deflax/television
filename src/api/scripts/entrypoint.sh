9b923e277e08afabf3e3dd02c9c545253ad4b9bc
Daniel Krastev <daniel@blackbear.bg>
2025-02-06 19:34:40 +0200

    Use the old waitress command

--- src/api/scripts/entrypoint.sh
+++ src/api/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -f /app/api.py ]; then
-    DEFAULT_MODULE_NAME=api
+    DEFAULT_MODULE_NAME=app.api
 fi
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-create_app}
