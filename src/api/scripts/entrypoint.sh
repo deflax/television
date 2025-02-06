50b090947eb2f03fce890e5c19ee5a74f71e9331
deflax <daniel@deflax.net>
2025-02-06 16:53:35 +0000

    use the create_api function call

--- src/api/scripts/entrypoint.sh
+++ src/api/scripts/entrypoint.sh
@@ -6,7 +6,7 @@ if [ -f /app/flask_api.py ]; then
     DEFAULT_MODULE_NAME=flask_api
 fi
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-VARIABLE_NAME=${VARIABLE_NAME:-api}
+VARIABLE_NAME=${VARIABLE_NAME:-create_api}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
 exec "$@"
