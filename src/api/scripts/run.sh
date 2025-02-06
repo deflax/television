45d0cf802432a0e21038f1c9f105b0063d45d9d4
deflax <daniel@deflax.net>
2025-02-06 19:38:57 +0200

    show current dir

--- src/api/scripts/run.sh
+++ src/api/scripts/run.sh
@@ -15,5 +15,5 @@ echo "waitress-serve $APP_MODULE"
 # exec waitress-serve --listen=*:8080 --trusted-proxy='*' \
 # --trusted-proxy-headers="x-forwarded-for","x-forwarded-host","x-forwarded-proto","x-forwarded-port" \
 # --log-untrusted-proxy-headers --threads=16 --call app.api:create_app
-
-waitress-serve --port=8080" --threads=16 --call $APP_MODULE"
+pwd
+waitress-serve --port=8080" --threads=16 --call $APP_MODULE
