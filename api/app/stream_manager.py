898b03e6fb1dda9a721fe6327f6954433ed7b860
deflax <daniel@deflax.net>
2026-02-01 18:22:04 +0200

    Use plain dict

--- api/app/stream_manager.py
+++ api/app/stream_manager.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from typing import Dict, Optional, Any
 from apscheduler.schedulers.background import BackgroundScheduler
 from core_client import Client
-from core_client.base.models.v3.process_command import ProcessCommandAction
+
 
 
 # Constants
@@ -85,8 +85,7 @@ class StreamManager:
             return {'success': False, 'message': f'Invalid command. Must be one of: {", ".join(valid_commands)}'}
 
         try:
-            action = ProcessCommandAction(command)
-            self.client.v3_process_put_command(id=process_id, command=action)
+            self.client.v3_process_put_command(id=process_id, command={"command": command})
             self.logger.info(f'Sent "{command}" command to process {process_id}')
             return {'success': True, 'message': f'Process {process_id} command "{command}" sent successfully.'}
         except Exception as e:
