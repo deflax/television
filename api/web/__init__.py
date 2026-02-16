97afae410dc7db5825db34bfb3d499933e4cc3c1
deflax <daniel@deflax.net>
2026-02-16 00:16:36 +0000

    Trim web package exports
    Keep only register_routes in the public web package API to avoid exporting internal implementation classes.

--- api/web/__init__.py
+++ api/web/__init__.py
@@ -1,5 +1,3 @@
 from web.routes import register_routes
-from web.timecode_manager import TimecodeManager
-from web.visitor_tracker import VisitorTracker
 
-__all__ = ['register_routes', 'TimecodeManager', 'VisitorTracker']
+__all__ = ['register_routes']
