f95cf414824a5275dcd256f4443d4ea329626992
deflax <daniel@deflax.net>
2026-03-06 10:47:16 +0000

    Remove timestamp from replay service log format

--- replay/app/config.py
+++ replay/app/config.py
@@ -7,7 +7,7 @@ import threading
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format='%(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
