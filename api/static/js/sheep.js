f0b61209bd99e9451a5e9c3163e7221201f31889
deflax <daniel@deflax.net>
2026-06-04 14:28:27 +0000

    Revert "Fine tune the max sheep count cap"
    This reverts commit 1c9401a28bbc3d94b597a7dccff65e2684cfccc5.

--- api/static/js/sheep.js
+++ api/static/js/sheep.js
@@ -358,7 +358,7 @@ window.SheepInternals = window.SheepInternals || {};
     manager.instances.push(sheep);
     sheep.ensureInitialized();
 
-    if (manager.instances.length > MAX_SHEEP_COUNT) {
+    if (manager.instances.length >= MAX_SHEEP_COUNT) {
       stopSpawnTimer();
     }
 
@@ -371,7 +371,7 @@ window.SheepInternals = window.SheepInternals || {};
     }
 
     const sheep = spawnSheep({ maxCount: MANUAL_MAX_SHEEP_COUNT });
-    const reachedCap = Boolean(sheep && manager.instances.length > MANUAL_MAX_SHEEP_COUNT);
+    const reachedCap = Boolean(sheep && manager.instances.length >= MANUAL_MAX_SHEEP_COUNT);
 
     if (reachedCap) {
       manager.manualCapResetPending = true;
@@ -385,7 +385,7 @@ window.SheepInternals = window.SheepInternals || {};
   }
 
   function startSpawnTimer() {
-    if (manager.spawnTimer || !manager.enabled || manager.instances.length > MAX_SHEEP_COUNT) {
+    if (manager.spawnTimer || !manager.enabled || manager.instances.length >= MAX_SHEEP_COUNT) {
       return;
     }
 
